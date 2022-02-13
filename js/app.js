1b4908200df70f60afd82bd6370d64ed089e5d19
mukarrom21 <mukarrom21@gmail.com>
2022-02-13 22:50:56 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+function getPin() {
+  const pin = Math.round(Math.random() * 10000);
+  const pinString = pin + "";
+  if (pinString.length == 4) {
+    return pin;
+  } else {
+    // console.log('got 3 digit',pin);
+    return getPin();
+  }
+}
+
+function generatePin() {
+  const pin = getPin();
+  document.getElementById("display-pin").value = pin;
+}
+
+document.getElementById("key-pad").addEventListener("click", function (event) {
+  const number = event.target.innerText;
+  const calcInput = document.getElementById("typed-numbers");
+  if (isNaN(number)) {
+    if (number == "C") {
+      calcInput.value = "";
+    }
+  } else {
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+  }
+});
+
+function verifyPin() {
+  const pin = document.getElementById("display-pin").value;
+
+  const typedNumber = document.getElementById("typed-numbers").value;
+  if (pin == typedNumber) {
+    document.getElementById("notify-success").style.display = "block";
+    document.getElementById("notify-fail").style.display = "none";
+  } else {
+    document.getElementById("notify-fail").style.display = "block";
+    document.getElementById("notify-success").style.display = "none";
+  }
+}
